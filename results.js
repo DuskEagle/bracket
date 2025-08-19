ae721f9b8d0eed9ce347033e771dada7f44e3713
Joel Kenny <joel@cockroachlabs.com>
2025-08-19 13:02:55 -0400

    michael rd 5

--- results.js
+++ results.js
@@ -9,7 +9,7 @@ Evan Tan,Edward Zhang,1,https://online-go.com/review/1521956
 Jeremiah Donley,Evan Tan,2,https://online-go.com/review/1522098
 Guanyu Song,Michael Xu,1,https://online-go.com/review/1522102
 Jeremiah Donley,Guanyu Song,2,https://online-go.com/review/1522549
-Michael Xu,Edward Zhang,0,https://online-go.com/review/1522548
+Michael Xu,Edward Zhang,1,https://online-go.com/review/1522548
 
 # Group B
 Qiyou Wu,Wanqi Zhu,1,https://online-go.com/review/1521383
